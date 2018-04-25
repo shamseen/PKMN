26e8d255bdb06a382f883c00d000a3228dc385a2
shamseen <shamseen.c@gmail.com>
2018-04-25 15:59:33 -0400

    tabs and mvvm skeleton

--- PKMN/PKMN/MainPage.xaml.cs
+++ PKMN/PKMN/MainPage.xaml.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace PKMN
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage ()
+        {
+            InitializeComponent();
+        }
+    }
 }
